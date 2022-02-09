3f6b59250bca04b1ae4851e1e0df83224b0c99cc
yanrivera19 <yan.rivera.6@gmail.com>
2022-02-09 12:10:54 -0800

    Changed ToDo text format from 'ToDo' to 'to-do' to make it more clear.

--- todo-app/src/components/HomePage.js
+++ todo-app/src/components/HomePage.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
-import {addTodo} from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { addTodo } from "../actions";
 
-const HomePage = props => {
+const HomePage = (props) => {
 	const buttonLabel = "Add";
 
-	const onSubmit = formValues => {
-		props.addTodo(formValues)
+	const onSubmit = (formValues) => {
+		props.addTodo(formValues);
 	};
 
 	return (
 		<>
-			<h1>ToDo List</h1>
-			<TodoForm buttonLabel={buttonLabel} onSubmit={onSubmit}/>
+			<h1>To-Do List</h1>
+			<TodoForm buttonLabel={buttonLabel} onSubmit={onSubmit} />
 			<TodoList />
 		</>
 	);
 };
 
-export default connect(null, {addTodo})(HomePage);
+export default connect(null, { addTodo })(HomePage);
